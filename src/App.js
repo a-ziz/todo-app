698b520550d10f72cb448514a89b9ba1a3154896
Aziz <azizahmed.dev@gmail.com>
2023-03-11 19:50:41 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import { AiOutlinePlus } from "react-icons/ai";
+import Todo from "./Todo";
+
+import { db } from "./firebase";
+import {
+  query,
+  collection,
+  onSnapshot,
+  updateDoc,
+  doc,
+  addDoc,
+  deleteDoc,
+} from "firebase/firestore";
+
+const style = {
+  bg: `h-screen w-screen p-2 bg-gradient-to-r from-[#2f80ed] to-[#1cb8e0]`,
+  container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-lg p-4`,
+  heading: `text-2xl font-bold text-center text-gray-800 p-2`,
+  form: `flex justify-between`,
+  input: `border p-2 w-full text-xl`,
+  button: `border p-4 ml-2 bg-blue-500 text-slate-100`,
+  count: `text-center p-2`,
+};
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+
+  // create todo
+  const createTodo = async (e) => {
+    e.preventDefault(e);
+
+    if (input === "") {
+      alert("Please enter a todo");
+      return;
+    }
+    await addDoc(collection(db, "todos"), {
+      text: input,
+      completed: false,
+    });
+    setInput("");
+  };
+
+  // read todo from firebase
+  useEffect(() => {
+    const q = query(collection(db, "todos"));
+    const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
+      let todosArray = [];
+      QuerySnapshot.forEach((doc) => {
+        todosArray.push({ ...doc.data(), id: doc.id });
+      });
+      setTodos(todosArray);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  // update todo in firebase
+  const toggleComplete = async (todo) => {
+    await updateDoc(doc(db, "todos", todo.id), {
+      completed: !todo.completed,
+    });
+  };
+
+  // delete todo
+  const deleteTodo = async (id) => {
+    await deleteDoc(doc(db, "todos", id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={style.bg}>
+      <div className={style.container}>
+        <h3 className={style.heading}>Todo App</h3>
+        <form onSubmit={createTodo} className={style.form}>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            type="text"
+            className={style.input}
+            placeholder="Add Todo"
+          />
+          <button className={style.button}>
+            {" "}
+            <AiOutlinePlus size={30} />
+          </button>
+        </form>
+        <ul>
+          {todos.map((todo, index) => (
+            <Todo
+              key={index}
+              todo={todo}
+              toggleComplete={toggleComplete}
+              deleteTodo={deleteTodo}
+            />
+          ))}
+        </ul>
+        {todos.length < 1 ? null : (
+          <p className={style.count}>{`You have ${todos.length} todos.`}</p>
+        )}
+      </div>
     </div>
   );
 }
